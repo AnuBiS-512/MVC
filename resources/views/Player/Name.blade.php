ec6f64b4a6e8e2355365bbefc33df2b7e4d8fc5e
Fatima AIT-BOUH <fatimaaitbouh20@gmail.com>
2023-12-15 12:12:36 +0100

    this is it

--- resources/views/Player/Name.blade.php
+++ resources/views/Player/Name.blade.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Fixed Page</title>
+    <title>nqme Page</title>
     <!-- Include Bootstrap CSS -->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
     <style>
